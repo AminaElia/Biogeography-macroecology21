1408511b7eb19f32658e5fa4e43ec716b4d8b804
AminaElia <73693978+AminaElia@users.noreply.github.com>
2021-05-12 14:27:53 +0200

    Update lab1.r

--- lab1.r
+++ lab1.r
@@ -1,2 +1,10 @@
 setwd("/users/Amina/Desktop/biogeo")
 dat <- read.csv("data_biomac_3.csv")
+library(vegan)
+dim(dat)
+com <- dat[, -(1:7)]
+rowSums(com > 0)
+hist(specnumber(com))
+elba <- dat[dat$island == "Elba", ]
+sum(specnumber(com, MARGIN = 2) == 1)
+head(sort(specnumber(com, MARGIN = 2), decreasing = T), 15)
