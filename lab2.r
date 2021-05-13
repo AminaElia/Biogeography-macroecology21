edb7319107c64b48c130d255d5769749202a7fb2
AminaElia <73693978+AminaElia@users.noreply.github.com>
2021-05-13 17:56:10 +0200

    Update lab2.r

--- lab2.r
+++ lab2.r
@@ -18,3 +18,29 @@ beta.multi(BCI_pa, index.family = "jaccard") # first is turnover, second is nest
 #turnover is much bigger so is dominant. dissimilarity!!
 d2 <- beta.pair(BCI_pa, index.family = "jaccard")
 boxplot(d2, names = c("Turnover", "Nestedness", "Total beta")) #50% of obseervation are inside the box
+
+data(BCI.env)
+bci_tur <- d2$beta.jtu #estraggo solo il turnover 
+bci_geo <- dist(BCI.env[, 1:2]) #voglio le due colonne UTM contenenti le coord, che sono la num 1 e num 2
+#result is a triangular distances' matrix
+plot(BCI.env$UTM.EW, BCI.env$UTM.NS) #scatterplot
+plot(bci_geo, bci_tur)
+#esce un plot con righe verticali. alla stessa geo distance con dissimilarity diverse. Sono pairs.
+#positive relation but maybe not significant
+mod1 <- lm(bci_tur ~ bci_geo) #model
+summary(mod1) #guarda Estimate(intercept e slope), Pr e Adj R-squared(quanto è espressivo il model)
+plot(bci_geo, bci_tur)
+abline(a = mod1$coefficients[1], b = mod1$coefficients[2], col = "red", lwd = 3)
+#grafico con linea del modello  a=intercept, b=slope
+
+#same for nestedness
+bci_nes <- d2$beta.jne #extract nestedness
+plot(bci_geo, bci_nes)
+mod2 <- lm(bci_nes ~bci_geo)
+summary(mod2) #i residue sono molto skwned, slope is negative, intercept is very small. Rsquared is extremely low.
+#il model non è ben espressivo, not significant (n.s.)
+# poca differenza tra distance e nestedness
+plot(bci_geo, bci_nes)
+abline(a = mod2$coefficients[1], b = mod2$coefficients[2], col = "red", lwd = 3)
+
+
